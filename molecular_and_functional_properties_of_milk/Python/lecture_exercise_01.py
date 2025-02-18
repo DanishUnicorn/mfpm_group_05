a0e1a022396ae3d1570293ad77423249ba323d6c
DanishUnicorn <132076490+DanishUnicorn@users.noreply.github.com>
2025-02-18 09:47:40 +0100

    Updated lecture_exercise_01 for better readability

--- molecular_and_functional_properties_of_milk/Python/lecture_exercise_01.py
+++ molecular_and_functional_properties_of_milk/Python/lecture_exercise_01.py
@@ -4,7 +4,7 @@
 """
 fat_globule_dia = 6.0*10**-6 # fat globule diameter in meters
 rho_milk_fat = 911.0 # density of milk fat in g/L
-rho_skim_milk = 1036.0 # density of skim milk in g/L
+rho_skim_milk = 1036.0 # density of skim milk in g/L 
 mu_skim_milk = 1.5*10**-3 # viscosity of skim milk in Pa·s (Remember to convert from mPa·s to Pa·s!)
 h_glass = 15.0 # height of the glass in cm 
 g = 9.81 # acceleration due to gravity in m/s^2
@@ -34,12 +34,8 @@ t_hour = t_min/60
 t_day = t_hour/24
 
 # Print the time taken for the fat globule to rise to the top of the glass
-print("velocity of the fat globule is {:.10f} m/s.".format(v_P))
-print("The time taken for the fat globule to rise to the top of the glass is {:.2f} seconds.".format(t))
-print("The time taken for the fat globule to rise to the top of the glass is {:.2f} minutes.".format(t_min))
-print("The time taken for the fat globule to rise to the top of the glass is {:.2f} hours.".format(t_hour))
-print("The time taken for the fat globule to rise to the top of the glass is {:.2f} days.".format(t_day))
-
+print(f"The velocity of the fat globule is {v_P:.10f} m/s.")
+print(f"Time taken for the fat globule to rise to the top of the glass: {t:.2f} s ({t_min:.2f} min, {t_hour:.2f} h, {t_day:.2f} days)")
 """
 # 2) How long is the creaming time if the glass is left in the fridge and cold agglutination has caused the formation of fat globule clusters with an average diameter of 60 μm? All other parameters remain unaffected:
 """
@@ -64,14 +60,11 @@ Delta_rho = rho_skim_milk - rho_milk_fat
 v_P_2 = (g*(fat_globule_dia_2**2)*Delta_rho)/(18*mu_skim_milk)
 
 # Calculate the time taken for the fat globule to rise to the top of the glass
-t = (h_glass/100)/v_P_2 # Convert height of the glass from cm to m
-t_min = t/60
-t_hour = t_min/60
-t_day = t_hour/24
+t_2 = (h_glass/100)/v_P_2 # Convert height of the glass from cm to m
+t_min_2 = t_2/60
+t_hour_2 = t_min_2/60
+t_day_2 = t_hour_2/24
 
 # Print the time taken for the fat globule to rise to the top of the glass
-print("velocity of the big fat globule is {:.10f} m/s.".format(v_P_2))
-print("The time taken for the big fat globule to rise to the top of the glass is {:.2f} seconds.".format(t))
-print("The time taken for the big fat globule to rise to the top of the glass is {:.2f} minutes.".format(t_min))
-print("The time taken for the big fat globule to rise to the top of the glass is {:.2f} hours.".format(t_hour))
-print("The time taken for the big fat globule to rise to the top of the glass is {:.2f} days.".format(t_day))
+print(f"The velocity of the big fat globule is {v_P_2:.10f} m/s.")
+print(f"Time taken for the big fat globule to rise to the top of the glass: {t_2:.2f} s ({t_min_2:.2f} min, {t_hour_2:.2f} h, {t_day_2:.2f} days)")
